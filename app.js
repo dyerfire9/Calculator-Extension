46ac6841595be7ac06e4a150530744d2578bb69f
dyerfire9 <canadianraza@yahoo.com>
2022-09-11 22:41:13 -0400

    Set up JS values

---
+++ app.js
@@ -0,0 +1,10 @@
+let display = document.querySelector(".calculator_display");
+// Select all operation elements
+let operations = document.querySelectorAll("[data-operation]");
+// Select all number elements
+let numbers = document.querySelectorAll("[data-number]");
+let equalButton = document.querySelector("[data-equals]");
+let clearButton = document.querySelector("[data-clear]");
+let decimalButton = document.querySelector("[data-decimal]");
+let deleteButton = document.querySelector("[data-delete]");
+
