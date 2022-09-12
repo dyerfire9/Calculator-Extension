8d6d149c95f080cb4253980aa21da58851b6c161
dyerfire9 <canadianraza@yahoo.com>
2022-09-12 00:01:06 -0400

    Created functionality for some buttons

--- app.js
+++ app.js
@@ -8,3 +8,62 @@ let clearButton = document.querySelector("[data-clear]");
 let decimalButton = document.querySelector("[data-decimal]");
 let deleteButton = document.querySelector("[data-delete]");
 
+// The target event property returns the element that triggered the event.
+let num1 = null;
+let op = null;
+let num2 = null;
+numbers.forEach(button => 
+    button.addEventListener("click", e => {
+            if (num1 === null){  
+                num1 = e.target.innerHTML;
+            } 
+            else{
+                num2 = e.target.innerHTML;
+            }
+            display.textContent += e.target.innerHTML;
+        }
+    )
+)
+
+operations.forEach(operation => 
+    operation.addEventListener("click", e => {
+        if (num1 != null && op === null){
+            display.textContent += e.target.innerHTML;
+            op = e.target.innerHTML;
+        }
+    })
+)
+equalButton.addEventListener("click", e => {
+    if (num1 != null && num2 != null){
+        if (op === '+'){
+            result = Number(num1) + Number(num2);
+        }
+        else if (op === '-'){
+            result = Number(num1) - Number(num2);
+        }
+        else if (op === ('*')){
+            result = Number(num1) * Number(num2);
+        }
+        else if (op === '÷'){
+            result = Number(num1) / Number(num2);
+        }
+        display.textContent = result;
+    }
+})
+
+clearButton.addEventListener("click", e => {
+    display.textContent = '';
+})
+
+decimalButton.addEventListener("click", e => {
+    display.textContent += e.target.innerHTML;
+})
+deleteButton.addEventListener("click", e => {
+    if(num1 != null && op === null)
+    
+    display.textContent = display.textContent.slice(0,-1)
+
+})
+
+
+
